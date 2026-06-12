0ee07f0e88729eb007aaacd19c43f5031d13451b
Jack Stehn <jack.stehn@protonmail.com>
2026-06-12 11:13:50 -0700

    remove extra reload bind

--- dots/.config/hypr/custom/keybinds.lua
+++ dots/.config/hypr/custom/keybinds.lua
@@ -1,5 +1,4 @@
 hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
-hl.bind("CTRL + SUPER + R", hl.dsp.exec_cmd("bash -c 'qs kill -c ${qsConfig:-ii} 2>/dev/null; sleep 0.3; qs -c ${qsConfig:-ii} & hyprctl reload'"), { description = "Reload: Restart Quickshell and reload Hyprland" })
 
 --# Scrolling layout
 hl.bind("SUPER + SHIFT + Apostrophe", hl.dsp.layout("fit all"), { description = "Layout: Fit all columns to screen evenly" })
